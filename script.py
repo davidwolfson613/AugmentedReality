c5a370caf72dfc22d55619635641bffebf0a3268
David Wolfson <46537678+davidwolfson613@users.noreply.github.com>
2022-04-20 18:27:31 -0400

    Update comments in script.py, clean up code

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import numpy as np
 def get_corners_list(image):
 
     """
-    List of image corner coordinates used in warping.
+    Get image corner coordinates used in warping.
     """
 
     height, width = image.shape[:2]
@@ -15,10 +15,10 @@ def get_corners_list(image):
 
 def video_frame_generator(filename):
 
-    # Open file with VideoCapture and set result to 'video'. (add 1 line)
+    # Open file with VideoCapture and set result to 'video'
     video = cv2.VideoCapture(filename)
-
-    # TODO
+    
+    # yield frames, one at a time, until no more frames in video
     while video.isOpened():
 
         status,frame = video.read()
@@ -40,14 +40,15 @@ def find_markers(image, template=None):
     Find corner markers in an image
     """
 
-    # USING CORNER DETECTOR WITH KMEANS CLUSTERING, AS OTHERS HAVE HAD SUCCESS WITH THIS METHOD
+    # USING CORNER DETECTOR WITH KMEANS CLUSTERING
     img = np.copy(image)
+    
     # need to convert image to grayscale then apply gaussian blur before passing the image to harris corner function
     gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     blur_img = cv2.GaussianBlur(gray_img,(11,11),0)
     temp = cv2.cornerHarris(blur_img,7,7,0.05) # these parameters need a lot of tuning to get them to work
 
-    # loc = np.where(temp>0.1*temp.max())
+    # get corners
     corners = np.array(list(zip(*np.where(temp>0.1*temp.max())[::-1]))).astype(np.float32) # thresholding then getting coordinates using zip
 
     '''
@@ -66,18 +67,18 @@ def find_markers(image, template=None):
     centers = np.array(first_centers + second_centers)
 
     # convert 2d array to list of tuples
-    centersT = centers.T
+    centersT = centers.T # tranpose corners
     out_list = list(zip(centersT[0],centersT[1]))
-    # print(out_list)
+
     return out_list
 
 
 def project_images(imageA, imageB, homography):
 
     """
-    Using the four markers in imageB, project imageA into the marked area.
+    This function projects imageA into the marked area of imageB.
 
-    Use your find_markers method to find the corners.
+    Uses find_markers method to find the corners.
     """
 
     # used this website as reference: https://stackoverflow.com/questions/46520123/how-do-i-use-opencvs-remap-function
@@ -104,7 +105,6 @@ def project_images(imageA, imageB, homography):
     warp_x,warp_y = warp[:-1]/warp[-1]
     warp_x = warp_x.reshape(h,w).astype(np.float32)
     warp_y = warp_y.reshape(h,w).astype(np.float32)
-    # print(warp_x)
 
     # do the remapping
     out_image = cv2.remap(src,warp_x,warp_y,cv2.INTER_LINEAR,dst=out_image,borderMode=cv2.BORDER_TRANSPARENT)
@@ -122,14 +122,14 @@ def project_images(imageA, imageB, homography):
 def find_homography(srcPoints, dstPoints):
 
     """
-    Solves for and returns a perspective transform.
+    This function performs a perspective transform on the provided points in the source and destination images.
 
-    Each source and corresponding destination point must be at the
-    same index in the lists.
     """
-
+       
+    # initialize the transform matrix to all zeros
     A = np.zeros((len(srcPoints)*2,9))
-
+    
+    # loop through source points and populate transform matrix
     for i in range(len(srcPoints)):
         for j in range(2):
             if j == 0:
@@ -139,7 +139,6 @@ def find_homography(srcPoints, dstPoints):
 
     # invert the A matrix and multiply with homogenous vector, then reshape to 3x3
     _,_,h = np.linalg.svd(A)
-    # print(h.shape)
     homography = h[-1,:]/h[-1,-1]
 
     return homography.reshape(3,3)
